6ef6d8223a6509222370345c7c5e32195d610ea1
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2025-06-10 21:40:08 -0700

    Add keyboard interrupt handling

--- client.py
+++ client.py
@@ -54,6 +54,8 @@ def main() -> None:
                     if text == "/quit":
                         break
                     send_q.put(text)
+        except KeyboardInterrupt:
+            print("KeyboardInterrupt received, exiting...")
         except Exception as e:
             print(f"unanticipated exception: {e}")
         finally:
